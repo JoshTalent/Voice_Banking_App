eeaf2e6f9591dcda6980f285e59a642732a96957
ntwari josue <134047313+JoshTalent@users.noreply.github.com>
2026-04-16 09:50:15 +0200

    Update main.js

--- web/js/main.js
+++ web/js/main.js
@@ -4,6 +4,7 @@
  * and open the template in the editor.
  */
 
+
 function viewPassword(){
     var password = document.getElementById("password");
     var changeIcon = document.getElementById("changeicon");
